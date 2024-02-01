04d7f2e689dd6711499b636d44dbba9b136313c6
Mary <gretchen7887@gmail.com>
2024-02-01 23:31:10 +0400

    Upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import {close, logo, menu} from '../assets'
+import {navLinks} from '../constants'
+
 
 const Navbar = () => {
   return (
-    <div></div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+<img src={logo} alt="hoobank"
+className='w-[124px]' />
+    </nav>
   )
 }
 
