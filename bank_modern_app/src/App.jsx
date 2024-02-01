1d19a3a510778f35de1a59b7f82f4c9c496a72a0
Mary <gretchen7887@gmail.com>
2024-02-01 22:08:43 +0400

    Upd

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React from 'react'
 
-import Navbar from './components/Navbar';
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components/index'
+
 import styles from './style'
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}><Navbar/></div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>Hero</div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing 
-        CardDeal 
-        Testimonials 
-        Clients 
-        CTA 
-        Footer
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+        <Clients /> <CTA /> <Footer />
       </div>
     </div>
   </div>
---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Business from './Business'
+import Clients from './Clients'
+import CTA from './CTA'
+import Stats from './Stats'
+import Footer from './Footer'
+import Testimonials from './Testimonials'
+
+export {
+  Navbar,
+  Hero,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Testimonials,
+  Footer,
+}
