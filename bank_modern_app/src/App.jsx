ff167590bb3183efcba4ba3084efd515652e16ac
Mary <gretchen7887@gmail.com>
2024-02-01 20:52:53 +0400

    Upd

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+
+import Navbar from './components/Navbar';
 import styles from './style'
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX}`}>
-      Meow
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}><Navbar/></div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing 
+        CardDeal 
+        Testimonials 
+        Clients 
+        CTA 
+        Footer
+      </div>
     </div>
   </div>
 )
