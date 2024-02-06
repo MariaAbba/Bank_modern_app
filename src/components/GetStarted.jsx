eaf3dcc2aebb181598a58af73782ecacd312ecf8
Mary <gretchen7887@gmail.com>
2024-02-06 14:36:50 +0400

    Upd

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 
 import styles from '../style'
 
@@ -12,15 +12,19 @@ const GetStarted = () => (
       className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
-        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>
+        </p>
           <img
             src={arrowUp}
             alt="arrowUp"
             className="w-[23px] h-[23px] object-contain"
           />
-        </p>
       </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+        
+      </p>
     </div>
   </div>
 )
