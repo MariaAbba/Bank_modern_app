ed384e126efa7846c6a49d0323f8b90f4531edbb
Mary <gretchen7887@gmail.com>
2024-02-06 11:38:15 +0400

    Upd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from '../style'
+import {discount, robot} from '../assets'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+<section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+  <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6` }></div>
+  </section>
+)
 
 export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
-const [toggle, setToggle] = React.useState(false)
-  
+  const [toggle, setToggle] = React.useState(false)
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -13,10 +13,9 @@ const [toggle, setToggle] = React.useState(false)
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins
-    font-normal
-    cursor-pointer
-    text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            }
     text-white
     `}
           >
@@ -26,12 +25,33 @@ const [toggle, setToggle] = React.useState(false)
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-      <img src={toggle ? close : menu } alt="menu" 
-      className='w-[28px] h-[28px] object-contain'
-      onClick={()=> setToggle((prev) => !prev)}/>
-      <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`} >
-
-      </div>
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins
+    font-normal
+    cursor-pointer
+    text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}
+    text-white
+    `}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )
