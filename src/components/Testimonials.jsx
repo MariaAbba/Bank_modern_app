c1c2e2b09740424808386bad3fe18ac71d0a544c
Mary <gretchen7887@gmail.com>
2024-02-06 18:28:11 +0400

    Upd

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => (
     id="clients"
     className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
   >
-    <div />
+    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
     <div className='w-full flex justify-between items-center md:flex-row flex-col sm"mb-16 mb-6 relative z-[1]'>
       <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
       <div className='w-full md:mt-0 mt-6'>
@@ -20,7 +20,7 @@ const Testimonials = () => (
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
       {feedback.map((card, index) => (
-        <Feedback
+        <FeedbackCard
         key={card.id}
         {...card}/>
       ))}
