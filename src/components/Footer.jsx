54910ff221e4b515dd10816c8d1d60fc27576de7
Mary <gretchen7887@gmail.com>
2024-02-06 21:10:02 +0400

    Upd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,21 +18,45 @@ const Footer = () => (
         </p>
       </div>
 
-      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap ,d:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div key={footerlink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-            <h4>
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap d:mt-0 mt-10">
+        {footerLinks.map((footerlink, index) => (
+          <div
+              key={index}
+            className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+          >
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
-            <ul>
+            <ul className="list-none mt-4">
               {footerlink.links.map((link, index) => (
-                <li key={link.name}
-                className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer'>
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                    index !== footerlink.links.length - 1 ? 'mb-4' : 'mb-0'
+                  }`}
+                >
                   {link.name}
                 </li>
               ))}
             </ul>
           </div>
+        ))}
+      </div>
+    </div>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        2024 HooBank. All Rights Reserved.
+      </p>
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'
+            }`}
+          />
         ))}
       </div>
     </div>
